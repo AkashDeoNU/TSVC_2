e277798355e5c5c8c8a74925b2c26786d09cd5ec
AkashDeoNU <AkashDeo2025@u.Northwestern.edu>
2025-12-23 10:07:56 +0530

    Initial

---
+++ src/s114.cpp
@@ -0,0 +1,69 @@
+
+/*
+ * This is an executable test containing a number of loops to measure
+ * the performance of a compiler. Arrays' length is LEN_1D by default
+ * and if you want a different array length, you should replace every
+ * LEN_1D by your desired number which must be a multiple of 40. If you
+ * want to increase the number of loop calls to have a longer run time
+ * you have to manipulate the constant value iterations. There is a dummy
+ * function called in each loop to make all computations appear required.
+ * The time to execute this function is included in the time measurement
+ * for the output but it is neglectable.
+ *
+ *  The output includes three columns:
+ *    Loop:        The name of the loop
+ *    Time(Sec):     The time in seconds to run the loop
+ *    Checksum:    The checksum calculated when the test has run
+ *
+ * In this version of the codelets arrays are static type.
+ *
+ * All functions/loops are taken from "TEST SUITE FOR VECTORIZING COMPILERS"
+ * by David Callahan, Jack Dongarra and David Levine except those whose
+ * functions' name have 4 digits.
+ */
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+
+#include "common.h"
+#include "array_defs.h"
+
+// array definitions
+__attribute__((aligned(ARRAY_ALIGNMENT))) real_t flat_2d_array[LEN_2D*LEN_2D];
+
+__attribute__((aligned(ARRAY_ALIGNMENT))) real_t x[LEN_1D];
+
+__attribute__((aligned(ARRAY_ALIGNMENT))) real_t a[LEN_1D],b[LEN_1D],c[LEN_1D],d[LEN_1D],e[LEN_1D],
+                                   aa[LEN_2D][LEN_2D],bb[LEN_2D][LEN_2D],cc[LEN_2D][LEN_2D],tt[LEN_2D][LEN_2D];
+
+__attribute__((aligned(ARRAY_ALIGNMENT))) int indx[LEN_1D];
+
+real_t* __restrict__ xx;
+real_t* yy;
+
+
+real_t s114(struct args_t * func_args)
+{
+
+//    linear dependence testing
+//    transpose vectorization
+//    Jump in data access - not vectorizable
+
+    initialise_arrays(__func__);
+    gettimeofday(&func_args->t1, NULL);
+
+    for (int nl = 0; nl < 200*(iterations/(LEN_2D)); nl++) {
+        for (int i = 0; i < LEN_2D; i++) {
+            for (int j = 0; j < i; j++) {
+                aa[i][j] = aa[j][i] + bb[i][j];
+            }
+        }
+        dummy(a, b, c, d, e, aa, bb, cc, 0.);
+    }
+
+    gettimeofday(&func_args->t2, NULL);
+    return calc_checksum(__func__);
+}
